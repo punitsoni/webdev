0f616ba5e81590541f4655ef8f9244a0e880d301
Punit Soni <punitxsmart@gmail.com>
2015-09-02 00:50:10 -0700

    updates

--- game/main.js
+++ game/main.js
@@ -20,58 +20,66 @@ $(function() {
     };
     var p1, p2;
 
+    /* player object constructor */
     function Player() {
-        return {
-            pos: { x: 0, y: 0 },
-            color: "blue",
-            v: { x: 0, y: 0 },
-            size: 10,
-            get bounds() {
-                return {
-                    top: this.pos.y,
-                    bottom: this.pos.y + this.size,
-                    left: this.pos.x,
-                    right: this.pos.x + this.size,
-                }
-            },
-
-            spawn: function (px, py) {
-                this.pos = {x: px, y: py};
-            },
-
-            update: function (time) {
-                this.pos.x += this.v.x * time;
-                this.pos.y += this.v.y * time;
-
-                /* keep it in canvas bounds */
-                if (this.bounds.left < 0)
-                    this.pos.x += -this.bounds.left;
-                if (this.bounds.right > canvasW)
-                    this.pos.x -= this.bounds.right - canvasW;
-                if (this.bounds.top < 0)
-                    this.pos.y += -this.bounds.top;
-                if (this.bounds.bottom > canvasH)
-                    this.pos.y -= this.bounds.bottom - canvasH;
-            },
-
-            render: function () {
-                ctx.fillStyle = this.color;
-                ctx.fillRect(Math.round(this.pos.x), Math.round(this.pos.y), this.size, this.size);
-            },
-        };
+        this.pos = { x: 0, y: 0 };
+        this.color = "blue";
+        this.v = { x: 20, y: 0 };
+        this.size = 10;
+        this.active = false;
     }
+
+    Object.defineProperty(Player.prototype, 'bounds', {
+        get: function() {
+            return {
+                top: this.pos.y,
+                bottom: this.pos.y + this.size,
+                left: this.pos.x,
+                right: this.pos.x + this.size,
+            }
+        }
+    });
+
+    Player.prototype.spawn = function(px, py) {
+        this.pos = {x: px, y: py};
+        this.active = true;
+    };
+
+    Player.prototype.update = function(time) {
+        if (this.active == false) return;
+
+        this.pos.x += this.v.x * time;
+        this.pos.y += this.v.y * time;
+
+        /* keep it in canvas bounds */
+        if (this.bounds.left < 0)
+            this.pos.x += -this.bounds.left;
+        if (this.bounds.right > canvasW)
+            this.pos.x -= this.bounds.right - canvasW;
+        if (this.bounds.top < 0)
+            this.pos.y += -this.bounds.top;
+        if (this.bounds.bottom > canvasH)
+            this.pos.y -= this.bounds.bottom - canvasH;
+    };
+
+    Player.prototype.render = function() {
+        if (this.active == false) return;
+        ctx.fillStyle = this.color;
+        ctx.fillRect(Math.round(this.pos.x),
+            Math.round(this.pos.y), this.size, this.size);
+    };
 
     function update(delta) {
         /* time in seconds, to be simulated */
         time = delta/1000;
         p1.update(time);
-        p2.update(time);
+        //p2.update(time);
     }
 
     function render() {
         ctx.clearRect(0, 0, canvasW, canvasH);
         p1.render();
-        p2.render();
+        //p2.render();
 
         /* display current fps */
         $("#fps").html(Math.round(fps));
@@ -104,18 +112,21 @@ $(function() {
     }
 
     function initialize() {
-        p1 = Player();
+        p1 = new Player();
         p1.color = "blue";
         p1.spawn(canvasW*0.25, canvasH*0.75);
 
-        p2 = Player();
-        p2.color = "green";
-        p2.spawn(canvasW*0.75, canvasH*0.75);
+        //p2 = Player();
+        //p2.color = "green";
+        //p2.spawn(canvasW*0.75, canvasH*0.75);
     }
 
     function startGame() {
         if (gameState == State.RUNNING) {
             return;
+        }
+        if (gameState == State.LOADED) {
+            initialize();
         }
         gameState = State.RUNNING;
         /* start main loop with a dummy first frame */
@@ -125,10 +136,10 @@ $(function() {
             lastFpsUpdate = timeStamp-1000;
             frameCount = 0;
             fps = 0;
-            initialize();
             reqFrameId = requestAnimationFrame(mainLoop);
         });
         $("#btnStart").html("Stop");
+        console.log("started");
     }
 
     function stopGame() {
@@ -141,6 +152,16 @@ $(function() {
         $("#btnStart").html("Start");
     }
 
+    function pauseGame() {
+        console.log("paused");
+        if (gameState != State.RUNNING) {
+            return;
+        }
+        cancelAnimationFrame(reqFrameId);
+        gameState = State.PAUSED;
+        $("#fps").html("0");
+    }
+
     $("#btnStart").click(function(){
         if (gameState == State.LOADED) {
             startGame();
@@ -148,6 +169,14 @@ $(function() {
             stopGame();
         }
     });
+
+    $(window).blur = function() {
+        pauseGame();
+    }
+
+    $(window).focus = function() {
+        startGame();
+    }
 
     gameState = State.LOADED;
     console.log("game loaded.");
